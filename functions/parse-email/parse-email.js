1d004852924b954265e430a3c3cd09cc0d3d8dca
David Thyresson <david@stattleship.com>
2019-12-30 18:22:01 -0500

    Test some identity and mailgun features

---
+++ functions/parse-email/parse-email.js
@@ -0,0 +1,27 @@
+const simpleParser = require("mailparser").simpleParser;
+const querystring = require("querystring");
+
+// const crypto = require('crypto')
+//
+// const verify = ({ apiKey, timestamp, token, signature }) => {
+//     const encodedToken = crypto
+//         .createHmac('sha256', apiKey)
+//         .update(timestamp.concat(token))
+//         .digest('hex')
+//
+//     return (encodedToken === signature)
+// }
+
+exports.handler = function(event, context, callback) {
+  const email = querystring.parse(event.body);
+
+  // console.log(email);
+  console.log(event.body);
+  console.log(email.subject);
+  console.log(email["message-url"]);
+
+  callback(null, {
+    statusCode: 200,
+    body: email["message-url"]
+  });
+};
